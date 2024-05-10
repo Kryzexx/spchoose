78b981edf379d7c7b50ef2aa85f7e36c19bf4373
a.m <126760638+Kryzexx@users.noreply.github.com>
2024-05-10 21:43:50 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+const btn = document.querySelector('.select-btn')
 function selectSpawn(spawnid) {
+    // choose border
     document.getElementById('spawn-choose').style.border = 'none'
     document.getElementById('home-choose').style.border = 'none'
     document.getElementById('fraction-choose').style.border = 'none'
     document.getElementById('exitpoint-choose').style.border = 'none'
     const choice = document.getElementById(spawnid)
     choice.style.border = "2px solid red"
+    btn.addEventListener('click', () => {
+        //cef.emit("pwd:choice", spawnid)
+        return DisplayError(spawnid)
+    })
+}
+
+function DisplayError(errormsg) {
+    Swal.fire({
+        title: "შეცდომა",
+        text: errormsg,
+        icon: "error"
+    });
 }
