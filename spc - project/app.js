c670e2e9b2b4224d589eff1e455330e11e5bd5ac
a.m <126760638+Kryzexx@users.noreply.github.com>
2024-05-10 20:02:37 +0400

    Delete spc - project directory

--- spc - project/app.js
+++
@@ -1,8 +0,0 @@
-function selectSpawn(spawnid) {
-    document.getElementById('spawn-choose').style.border = 'none'
-    document.getElementById('home-choose').style.border = 'none'
-    document.getElementById('fraction-choose').style.border = 'none'
-    document.getElementById('exitpoint-choose').style.border = 'none'
-    const choice = document.getElementById(spawnid)
-    choice.style.border = "2px solid red"
-}
